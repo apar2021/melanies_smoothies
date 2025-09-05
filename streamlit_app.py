a94eeb798d60336592820c390a19c2c4917206ae
apar2021 <45519735+apar2021@users.noreply.github.com>
2025-09-05 10:08:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()                                                                      
 
-pd_df =my_dataframe.to_pandas()                                                                      
+pd_df = my_dataframe.to_pandas()                                                                      
                                                                     
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
